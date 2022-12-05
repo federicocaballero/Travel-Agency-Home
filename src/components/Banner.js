059d87da399fade58d4804e00f5b74e6d3053f8d
Federico Caballero <fedecaballero1234@gmail.com>
2022-12-05 13:52:53 -0300

    Se agregaron estilos al componente Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,104 +4,78 @@ const Banner = () => {
     const continent = ['Asia','America', 'Africa', 'Europa','Oceania'];
     const asiaCountry = ['el colo', '231', 'doha','qatar'];
     const americaCountry = ['Argentina', 'Uruguay', 'Paraguay'];
-    // {     continent.forEach(function(element) {
-    //     console.log(element);
-    //     });
-    // } 
 
     return(
         <>
-        <div className="container background-image-banner">
-        <ul class="nav nav-pills nav-justified">
-            <li class="nav-item">
-                 {/* boton */}
-                <div>
-                <div class="btn-group w-100">
-                <button type="button" class="btn btn-light dropdown-toggle text-start" data-bs-toggle="dropdown" aria-expanded="false">
-                    Continent
-                </button>
-                <ul class="dropdown-menu">
-                    {continent.map((continent) =>
-                    <li>{continent}</li>)
-                    }                    
-                </ul>
-                </div>
-                </div>
-            {/* boton */}
-            </li>
-            <li class="nav-item">
-                 {/* boton */}
-                <div>
-                <div class="btn-group w-100">
-                <button type="button" class="btn btn-light dropdown-toggle text-start" data-bs-toggle="dropdown" aria-expanded="false">
-                    All Countrys
-                </button>
-                <ul class="dropdown-menu">
-                    {asiaCountry.map((asiaCountry) =>
-                    <li>{asiaCountry}</li>)
-                    }                  
-                </ul>
-                </div>
-                </div>
-            {/* boton */}
-            </li>
-            <li class="nav-item">
-                 {/* boton */}
-                <div>
-                <div class="btn-group w-100">
-                <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    Action
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    <li><hr class="dropdown-divider"/></li>
-                    <li><a class="dropdown-item" href="#">Separated link</a></li>
-                </ul>
-                </div>
-                </div>
-            {/* boton */}
-            </li>
-            <li class="nav-item">
-                 {/* boton */}
-                <div>
-                <div class="btn-group w-100">
-                <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    Action
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    <li><hr class="dropdown-divider"/></li>
-                    <li><a class="dropdown-item" href="#">Separated link</a></li>
-                </ul>
-                </div>
-                </div>
-            {/* boton */}
-            </li>
-        </ul>
-            
-            
-            
-            
-            {/* boton */}
-            <div>
-            <div class="btn-group">
-            <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                Action
-            </button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                <li><hr class="dropdown-divider"/></li>
-                <li><a class="dropdown-item" href="#">Separated link</a></li>
-            </ul>
-            </div>
-            </div>
-            {/* boton */}
+        <div className="background-image-banner p-5 d-flex flex-column align-items-center justify-content-center">
+            <p className="lh-1 text-center m-1">
+                <p class="fs-2 text-light text-uppercase fw-semibold m-0">let us take you to Your</p>
+                <p class="fs-1 text-light text-uppercase fw-bold">dream destinations</p>
+            </p>
+            <ul class="nav nav-pills nav-justified w-75">
+                {/*CONTINENT*/}
+                <li class="nav-item">
+                    {/* boton */}
+                    <div className="">
+                    <div class="btn-group w-100 ">
+                    <button type="button" class="btn btn-light dropdown-toggle text-start borde-izquierdo-redondo p-3" data-bs-toggle="dropdown" aria-expanded="false">
+                        Continent
+                    </button>
+                    <ul class="dropdown-menu">
+                        {continent.map((continent) =>
+                        <li>{continent}</li>)
+                        }                    
+                    </ul>
+                    </div>
+                    </div>
+                {/* boton */}
+                </li>
+                {/*COUNTRYS*/}
+                <li class="nav-item">
+                    {/* boton */}
+                    <div>
+                    <div class="btn-group w-100">
+                    <button type="button" class="btn btn-light dropdown-toggle text-start bordes-rectos p-3" data-bs-toggle="dropdown" aria-expanded="false">
+                        All Countrys
+                    </button>
+                    <ul class="dropdown-menu">
+                        {asiaCountry.map((asiaCountry) =>
+                        <li>{asiaCountry}</li>)
+                        }                  
+                    </ul>
+                    </div>
+                    </div>
+                {/* boton */}
+                </li>
+                {/*BUDGET*/}
+                <li class="nav-item">
+                    {/* boton */}
+                    <div>
+                    <div class="btn-group w-100">
+                    <button type="button" class="btn btn-light dropdown-toggle bordes-rectos p-3" data-bs-toggle="dropdown" aria-expanded="false">
+                        Your budget ($)
+                    </button>
+                    <ul class="dropdown-menu">
+                    <label for="customRange1" class="form-label">Budget: </label>
+                    <input type="range" class="form-range" id="customRange1"/>
+                    </ul>
+                    </div>
+                    </div>
+                {/* boton */}
+                </li>
+                {/*SEARCH*/}
+                <li class="nav-item">
+                    {/* boton */}
+                    <div>
+                    <div class="btn-group w-100">
+                    <button type="button" class="btn btn-success borde-derecho-redondo p-3" data-bs-toggle="dropdown" aria-expanded="false">
+                        Find your tours!!
+                    </button>
+                    </div>
+                    </div>
+                {/* boton */}
+                </li>
+            </ul>  
         </div>
         
         </>
